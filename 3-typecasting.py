b59f03f35b80ae7391b76aefac3c5c6cfb5f8bcd
Sanjay Karmakar <sanjaykarmakar86@gmail.com>
2026-04-21 09:45:13 +0530

    Added serial number to TypeCasting

---
+++ 3-typecasting.py
@@ -0,0 +1,18 @@
+a = "1"
+b = "2"
+
+print(a + b);           # output: 12
+
+print(int(a) + int(b))  # output: 3
+
+
+# Typecasting   => The conversion of one data type to another data type is called typecasting. It is used to convert the data from one type to another type.
+
+# Types of Typecasting
+
+# 1. Implicit   => The conversion of one data type to another data type is done by the interpreter automatically. It is done when we perform operations on different data types.
+
+# 2. Explicit   => The conversion of one data type to another data type is called explicit typecasting. It is done by the programmer.
+
+
+# Python support a wide range of built-in functions for typecasting. Some of the commonly used typecasting functions are: int(), float(), str(), bool(), list(), tuple(), set(), dict().
