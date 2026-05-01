d4515cfb236bf3681ff3bde8eeb7b6bee948ac3b
Sanjay Karmakar <sanjaykarmakar86@gmail.com>
2026-05-01 19:23:05 +0530

    Strings in Python

---
+++ 5-strings.py
@@ -0,0 +1,19 @@
+# Strings in Python
+name = "Radhe Krishna"
+print("Hello " + name)  # Output: Hello Radhe Krishna
+
+multiLineString = """This is a multi-line string.
+It can span multiple lines."""
+print(multiLineString)
+
+# Output:   This is a multi-line string.
+#           It can span multiple lines.
+
+multiLineString1 = '''This is a multi-line string.
+It can span multiple lines.'''
+print(multiLineString1)
+
+# Output:   This is a multi-line string.
+#           It can span multiple lines.
+
+print(name[3])  # Output: h
